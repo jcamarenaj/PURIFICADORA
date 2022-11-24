f045c6c9d9d756fe6b73fe2eca0fffab6916e06c
juan.jose <juan_camrena@hotmial.com>
2022-11-24 15:56:05 -0600

    proyecto

---
+++ laravelvue2/resources/home.blade.php
@@ -0,0 +1,29 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+<h1>table</h1>
+@stop
+
+@section('content')
+<div class="card">
+    <div class="card-header">
+        <h1 class="card-title"> hola </h1>
+</div>
+<div class"card-body">
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem animi, ullam est repellendus vitae saepe, consequuntur architecto ad, ipsa quas rerum. Cumque alias vel odio amet error voluptate repellat dolore!
+</div>
+</div>
+@stop
+@section('css')
+<link rel="stylesheet" href="/css/admin_custom.css"
+@stop
+
+@section('css')
+<link rel="stylesheet" href="/css/admin_custom.css">
+
+@stop
+@section('js')
+<script> console.log('Hi!'); </script>
+@stop
---
+++ laravelvue2/resources/js/Pages/Productos/Create.vue
@@ -0,0 +1,118 @@
+<script setup>
+    import BreezeAuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+    import { Head } from "@inertiajs/inertia-vue3";
+    import BreezeButton from "@/Components/PrimaryButton.vue";
+    import { Link } from "@inertiajs/inertia-vue3";
+    import { useForm } from "@inertiajs/inertia-vue3";
+
+    const props = defineProps({
+        productos: {
+            type: Object,
+            default: () => ({}),
+        },
+    });
+
+
+    const form = useForm({
+        name: '',
+        namem: '',
+        id: '',
+
+    });
+
+    const submit = () => {
+        form.post(route("productos.store"));
+    };
+    </script>
+
+    <template>
+        <Head title="Agregar producto" />
+
+        <BreezeAuthenticatedLayout>
+            <template #header>
+                <h2 class="text-xl font-semibold leading-tight text-gray-800">
+                    Agregar producto
+                </h2>
+            </template>
+
+            <div class="py-12">
+                <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                        <div class="p-6 bg-white border-b border-gray-200">
+                            <form @submit.prevent="submit">
+                                <div class="mb-6">
+                                    <label
+                                        for="Name"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                                        >Nombre</label
+                                    >
+                                    <input
+                                        type="text"
+                                        v-model="form.name"
+                                        name="name"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                        placeholder="Nombre de la persona"
+                                    />
+                                    <div
+                                        v-if="form.errors.name"
+                                        class="text-sm text-red-600"
+                                    >
+                                        {{ form.errors.name }}
+                                    </div>
+                                </div>
+                                <div class="mb-6">
+                                    <label
+                                        for="Age"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                                        >Marca</label
+                                    >
+                                    <input
+                                        type="number"
+                                        v-model="form.namem"
+                                        name="name"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                        placeholder="Indique la edad en años de la persona"
+                                    />
+                                    <div
+                                        v-if="form.errors.namem"
+                                        class="text-sm text-red-600"
+                                    >
+                                        {{ form.errors.namem }}
+                                    </div>
+                                </div>
+                                <div class="mb-6">
+                                    <label
+                                        for="Telephone"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                                        >Inventario</label
+                                    >
+                                    <input
+                                        type="number"
+                                        v-model="form.id"
+                                        name="id"
+                                        id=""
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                    >
+
+                                    <div
+                                        v-if="form.errors.id"
+                                        class="text-sm text-red-600"
+                                    >
+                                        {{ form.errors.id }}
+                                    </div>
+                                </div>
+                                <button
+                                    type="submit"
+                                    class="text-white bg-blue-700  focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 "
+                                    :disabled="form.processing"
+                                    :class="{ 'opacity-25': form.processing }"
+                                >
+                                    Registrar producto
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </BreezeAuthenticatedLayout>
+    </template>
