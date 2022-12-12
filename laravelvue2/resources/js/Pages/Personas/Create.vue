8ca7d5d67d917facaed00b5e5a6254390a71b4b5
juan.jose <juan_camrena@hotmial.com>
2022-12-12 15:57:50 -0600

    avances2

--- laravelvue2/resources/js/Pages/Personas/Create.vue
+++ laravelvue2/resources/js/Pages/Personas/Create.vue
@@ -64,14 +64,14 @@
                                     <label
                                         for="Age"
                                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                                        >Edad</label
+                                        >fecha</label
                                     >
                                     <input
                                         type="number"
                                         v-model="form.age"
                                         name="age"
                                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                        placeholder="Indique la edad en años de la persona"
+                                        placeholder="fecha con numeros"
                                     />
                                     <div
                                         v-if="form.errors.age"
